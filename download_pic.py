09f8e1394426448e316c7911b36445a71c6a4f99
zhaojiajin <zhaojiajin@gmail.com>
2017-06-17 14:34:34 +0800

    comment

--- download_pic.py
+++ download_pic.py
@@ -4,16 +4,33 @@ import os
 import requests
 import re
 import urllib
+
+
+"""
+下载策略：
+1，初始化时下载第一页的壁纸，并记录
+2，每次更新壁纸检查第一页的第一张有没有更新，如果有，下载并更换
+3，需要记录第一张壁纸的名称，需要记录最后一张壁纸的页码和位置
+4，如果没有更新的话，从记录的最后一张壁纸开始下载，
+
+
+"""
+
+
 page = 0
 base = "http://wallpaperswide.com"
 url = '{1}/top_wallpapers/page/{0}'.format(page,base)
 resolution = ['1920x1080','2048x1152','2400x1350','2560x1440','2880x1620','3554x1999']
 
 
+first = ""
+pic_idx = 1
+
+
 def download_pic(link):
-    pic_name = link.split('/')[-1]
-    urllib.request.urlretrieve('{0}{1}'.format(base,link),'wallpaper/{0}'.format(pic_name))
-    print (pic_name)
+    pic_name = "wallpaper/" + link.split('/')[-1]
+    urllib.request.urlretrieve('{0}{1}'.format(base,link),pic_name)
+    return pic_name
 
 
 def parse_rec(data):
@@ -42,7 +59,7 @@ res = pa.findall(data.text)
 for rec in res:
     pic_page_url = parse_rec(rec)
     pic_link = get_pic(pic_page_url)
-    download_pic(pic_link)
+    pic_name = download_pic(pic_link)
 
 
 
