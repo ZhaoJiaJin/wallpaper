30b512ade178dbc037f0d31139274f39e2aa09e4
zhaojiajin <zhaojiajin@gmail.com>
2017-06-17 13:14:26 +0800

    get

--- download_pic.py
+++ download_pic.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import os
+import requests
+
+page = 0
+url='http://wallpaperswide.com/top_wallpapers/page/{0}'.format(page)
 
 
-os.system("feh --randomize --bg-fill /home/jin/Pictures/*.jpg")
+data = requests.get(url)
+print (data.text)
+
+
+
+
+#os.system("feh --randomize --bg-fill /home/jin/Pictures/*.jpg")
